3d07a823d439cb28ca809a34261e3b5b0c159cfb
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-11-19 11:29:08 +0530

    update project

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,21 +1,29 @@
+// const express = require("express");
+// const router = express.Router();
+// const upload = require("../models/product-model");
+// const productModel = require("../config/multer-config");
+
+// router.post("/create", upload.single("image"), async function (req, res) {
+//  let { image,
+//    namne,
+//    price,
+//    discount,
+//    bgcolor,
+//    panelcolor,
+//    textcolor,
+//   } = req.body;
+
+//   let product = await productModel.create({
+//     image:req.file.buffer,
+//   })
+// });
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
-const upload = require("../models/product-model");
-const productModel = require("../config/multer-config");
 
-router.post("/create", upload.single("image"), async function (req, res) {
- let { image,
-   namne,
-   price,
-   discount,
-   bgcolor,
-   panelcolor,
-   textcolor,
-  } = req.body;
-
-  let product = await productModel.create({
-    image:req.file.buffer,
-  })
+router.get("/", function (req, res) {
+  res.send("hey it's working");
 });
 
 module.exports = router;
