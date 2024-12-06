f2b010db7fbbaef96f78cbf4f4cd85e2473f141f
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-12-06 10:05:18 +0530

    code update

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../models/product-model");
-const productModel = require("../config/multer-config");
+const multer = require("multer");
+const productModel = require("../models/product-model");
+
+
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
 
 router.post("/create", upload.single("image"), async function (req, res) {
- try { 
-       let {  name, price, discount, bgcolor, panelcolor, textcolor} = req.body;
+  try {
+    let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
 
-       let product = await productModel.create({
-        image:req.file.buffer,
-          name,
-          price,
-          discount,
-          bgcolor,
-          panelcolor,
-          textcolor,
-        });
-        req.flash("success", "product is successfully")
-        res.redirect("/owners/admin");
-      } catch(err){
-         res.send(err.message);
+    
+    let product = await productModel.create({
+      image: req.file.buffer, 
+      name,
+      price,
+      discount,
+      bgcolor,
+      panelcolor,
+      textcolor,
+    });
 
-        }
-
+    req.flash("success", "Product successfully created");
+    res.redirect("/owners/admin");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
-
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const isloggedin = require("../middlewares/isLoggedIn");
+const isLoggedIn = require("../middlewares/isLoggedIn");
 const {
   registerUser,
   loginUser,
